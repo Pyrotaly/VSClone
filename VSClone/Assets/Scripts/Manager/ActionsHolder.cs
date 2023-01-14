b95c4d33f4185a887446cdaabfaa71ed3fce9a69
Pyrotaly <102985554+Pyrotaly@users.noreply.github.com>
2023-01-14 00:48:08 -0500

    MakingGameStructureOrganized

---
+++ VSClone/Assets/Scripts/Manager/ActionsHolder.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ActionsHolder
+{
+    public static Action OnEnemyKilled;
+}
+
