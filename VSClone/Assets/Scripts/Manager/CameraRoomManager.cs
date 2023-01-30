8215a87cd953e68d93c0c9aefd21ed2f42a15f3b
Pyrotaly <102985554+Pyrotaly@users.noreply.github.com>
2023-01-29 22:48:38 -0500

    TryingToWorkOnCamera

--- VSClone/Assets/Scripts/Manager/CameraRoomManager.cs
+++ VSClone/Assets/Scripts/Manager/CameraRoomManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // Old Script, used when transitioning between rooms like Binding of Issac
-public class CameraManager : MonoBehaviour
+public class CameraRoomManager : MonoBehaviour
 {
     public GameObject virtualCam;
 
