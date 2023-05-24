0067f1b5baa6dfd9060dbc2af41dec528cb7e4ac
Pyrotaly <102985554+Pyrotaly@users.noreply.github.com>
2023-05-24 14:36:56 -0400

    UpdatingArt

---
+++ VSClone/Assets/Scripts/Manager/CameraResolutionManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GenericCameraResolutionManager
+{
+    [RequireComponent(typeof(Camera))]
+    [ExecuteAlways]
+    public class CameraResolutionManager : MonoBehaviour
+    {
+        public bool maintainWidth = true;
+
+        [Range(-1, 1)]
+
+        public int adaptPosition;
+        float defaultWeidth, defaultHeight;
+
+        Vector3 CameraPos;
+        void Start()
+        {
+            Debug.Log(Camera.main.aspect);
+
+            Debug.Log(Camera.main.orthographicSize);
+
+            CameraPos = Camera.main.transform.position;
+            defaultHeight = Camera.main.orthographicSize;
+            defaultWeidth = Camera.main.orthographicSize * Camera.main.aspect;
+        }
+
+        void Update()
+        {
+            if (maintainWidth)
+            {
+                Camera.main.orthographicSize = defaultWeidth / Camera.main.aspect;
+
+                Camera.main.transform.position = new Vector3(CameraPos.x, adaptPosition * (defaultHeight - Camera.main.orthographicSize), CameraPos.z);
+            }
+            else
+            {
+                Camera.main.transform.position = new Vector3(adaptPosition * (defaultWeidth - Camera.main.orthographicSize * Camera.main.aspect), CameraPos.y, CameraPos.z);
+            }
+        }
+    }
+}
